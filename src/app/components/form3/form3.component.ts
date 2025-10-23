4372dba8ca33cb89ff7065a26b61239ced494021
yaviracjorge <jlr.corella@yavirac.edu.ec>
2025-10-23 14:49:38 -0500

    validacion antes del arroba y despues y boton funcional

--- src/app/components/form3/form3.component.ts
+++ src/app/components/form3/form3.component.ts
@@ -1,7 +1,11 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
-import {EqualPasswordValidator, SearchValidator} from "../../../utils/validators";
-import {IonInput, IonItem, IonLabel, IonList} from "@ionic/angular/standalone";
+import {
+  emailDomainValidator, emailUserValidator,
+  EqualPasswordValidator,
+  SearchValidator
+} from "../../../utils/validators";
+import {IonButton, IonInput, IonItem, IonLabel, IonList, IonText} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-form3',
@@ -12,25 +16,30 @@ import {IonInput, IonItem, IonLabel, IonList} from "@ionic/angular/standalone";
     IonItem,
     IonLabel,
     IonInput,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    IonText,
+    IonButton
   ]
 })
 export class Form3Component  implements OnInit {
 
   formBuilder = inject(FormBuilder)
   form = this.formBuilder.group({
-    email: ['',[Validators.required,Validators.email,Validators.pattern('^[a-z0-9_]+@(gmail|hotmail)\\.(com|es)$')]],
-    password: ['',[Validators.required,Validators.minLength(6),Validators.maxLength(10),Validators.pattern('^[a-z0-9]+[-_*@./+]$'),SearchValidator.validatorPassword]],
+    email: ['',[Validators.required,emailUserValidator,emailDomainValidator,Validators.email]],
+    password: ['',[Validators.required,Validators.minLength(6),Validators.maxLength(10),Validators.pattern('^[a-z0-9]+[-_*@./+]$'),SearchValidator]],
     confirmPassword: ['',[Validators.required]],
+
   },{
-    validators:[EqualPasswordValidator.validatorPassword('password','confirmPassword')]
+    validators:[EqualPasswordValidator('password','confirmPassword')]
   })
 
   get email() { return this.form.controls['email']; }
   get password() { return this.form.controls['password']; }
   get confirmPassword() { return this.form.controls['confirmPassword']; }
+
   constructor() { }
 
   ngOnInit() {}
 
+  protected readonly Error = Error;
 }
--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,6 +1,6 @@
 import {AbstractControl} from "@angular/forms";
 
-export class EqualPasswordValidator {
+/*export class EqualPasswordValidator {
    static validatorPassword(firstControl:string,secondControl:string){
     return (control:AbstractControl)=>{
       const firstValue = control.get(firstControl)?.value;
@@ -12,9 +12,20 @@ export class EqualPasswordValidator {
       return null;
     }
    }
+}*/
+export function EqualPasswordValidator(firstControl:string,secondControl:string){
+  return (control:AbstractControl)=>{
+    const firstValue = control.get(firstControl)?.value;
+    const secondValue = control.get(secondControl)?.value;
+
+    if (firstValue !== secondValue){
+      return {notEqual:true};
+    }
+    return null;
+  }
 }
 
-export class SearchValidator{
+/*export class SearchValidator{
   static validatorPassword(control:AbstractControl){
     const value = control.value;
     if(value == null){
@@ -27,23 +38,53 @@ export class SearchValidator{
     }
     return {notSpecialChart:true};
   }
-}
-/*export class SearchValidator{
-  static validatorPassword(first:string){
-    return (control:AbstractControl)=>{
-      const firstValue = control.get(first)?.value;
-      if(firstValue == null){
-        return null
-      }
-      const specialChart = /[@./*+!?¿_]/;
-      const hasSpecial = specialChart.test(firstValue);
-      if(hasSpecial){
-        return null;
-      }
-      return {notSpecial:true};
-    }
-  }
 }*/
+
+export function SearchValidator(control:AbstractControl) {
+  const value = control.value;
+  if(value == null){
+    return null;
+  }
+  const specialChart = /[@./*+!?¿_]/;
+  const hasSpecialChart = specialChart.test(value);
+  if(hasSpecialChart){
+    return null;
+  }
+  return {notSpecialChart:true};
+}
+
+export  function emailUserValidator(control:AbstractControl) {
+  const value = control.value;
+  if((value == null)|| value.indexOf("@") === -1){
+    return null;
+  }
+  const part = value.split("@");
+  const username = part[0];
+  const chart = /^[a-z]+[a-z0-9_]*$/
+  const hasChart = chart.test(username);
+  if(hasChart){
+    return null;
+  }
+  return {notChart:true};
+}
+
+export function emailDomainValidator(control:AbstractControl) {
+  const value = control.value;
+  if((value == null)|| value.indexOf("@") === -1){
+    return null;
+  }
+  const part = value.split("@");
+  const email = part[1];
+  const chart = /^(gmail|hotmail)\.(com|es)$/
+  const hasDomain = chart.test(email);
+  if(hasDomain){
+    return null;
+  }
+  return {notDomain:true};
+}
+
+
+
 
 /*const value = control.value;
  if (value == null){
